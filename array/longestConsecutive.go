7175130d04ac14b219a66275d158af0fddefeec0
nawafilhusnul <nawafilhusnul@gmail.com>
2023-09-02 18:46:40 +0700

    add longestConsecutive

--- array/longestConsecutive.go
+++ array/longestConsecutive.go
@@ -1,6 +1,31 @@
 package main
 
 func longestConsecutive(nums []int) int {
-	var res int
+	set := map[int]bool{}
+
+	for _, num := range nums {
+		set[num] = true
+	}
+
+	res := 0
+
+	for _, num := range nums {
+		if set[num-1] {
+			continue
+		}
+
+		sequence := 1
+		temp := num + 1
+
+		for set[temp] {
+			sequence++
+			temp++
+		}
+
+		if sequence > res {
+			res = sequence
+		}
+	}
+
 	return res
 }
